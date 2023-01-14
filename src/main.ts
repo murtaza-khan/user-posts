795690f7649b957f1fd093eec52e890eef83fcbe
murtaza-khan <murtazakhancs@gmail.com>
2023-01-14 18:52:45 +0500

    cors *

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors:true });
-  app.enableCors({origin:'*'});
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
